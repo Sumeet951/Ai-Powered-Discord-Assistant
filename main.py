5434872c2f96727e40c972262483fb599ef8c82f
Sumeet951 <sg297979@gmail.com>
2026-03-01 18:28:56 +0530

    Langchain Integrate

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import os
 import discord
-
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_agent
+from langchain.messages import HumanMessage
 # Load environment variables
 load_dotenv()
 
@@ -12,10 +14,22 @@ intents.message_content = True
 # Create client
 client = discord.Client(intents=intents)
 
+model=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+agent=create_agent(model=model,tools=[])
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    await message.channel.send('Hello from the bot!')
+    content=message.content
+    response=agent.invoke(
+        {
+            "messages":[HumanMessage(content)]
+        }
+    )
+    agent_message=response["messages"][-1].content
+    await message.channel.send(agent_message)
+
+    
 # Run bot with token
 client.run(os.getenv("DISCORD_API_KEY"))
--- readme.txt
+++ readme.txt
@@ -1,1 +1,4 @@
 Install Packages pip install discord.py
+LangChain Integrate Packages
+ pip install langchain-google-genai
+ pip install langchain
